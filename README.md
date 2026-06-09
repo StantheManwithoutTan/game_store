3ef338d3adca9f9c64555318c2a71f94df8e59e2
SGtrinkets <gomezstanley754@gmail.com>
2026-06-09 07:39:13 -0400

    test: add new login feature

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # game_store
+
+
+
+
+Para correr proyecto inicial:
+
+cd backend
+flask --app app run 
+
--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 # crea la aplicacion de flask
 app = Flask(__name__)
 
 # asigna la ruta de '/' al url de la funcion
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['username']
+        return f"Hello {name}, POST request received"
+    return render_template('name.html')
 
 # inicia el servidor local
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
