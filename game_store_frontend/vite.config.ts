535bad9e58c9ff0f225f1a1bb3f281a177423389
SGtrinkets <gomezstanley754@gmail.com>
2026-07-24 12:23:59 -0400

    feat: frontend actualizado con plugins de prueba

--- game_store_frontend/vite.config.ts
+++ game_store_frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest/config" />
 
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
+  },
 })
