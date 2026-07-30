28e00e9e7e742e5b072b2cb88c741e2ec0c90ce8
SGtrinkets <gomezstanley754@gmail.com>
2026-07-30 13:55:07 -0400

    fix: vitest archivo creado separado de vite.config.ts

--- game_store_frontend/vite.config.ts
+++ game_store_frontend/vite.config.ts
@@ -1,17 +1,6 @@
-/// <reference types="vitest/config" />
-
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {
-    include: ['src/**/*.{test,spec}.{ts,js,tsx,jsx}'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      reportsDirectory: './coverage',
-      include: ['src/**/*.ts'],
-    },
-  },
 })
