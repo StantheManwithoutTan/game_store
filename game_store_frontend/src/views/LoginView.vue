623ea7984a543ed88233e390c91506fc5f9cd2a1
SGtrinkets <gomezstanley754@gmail.com>
2026-07-29 20:29:11 -0400

    fix: forzar a loginview leer las variables de .env

--- game_store_frontend/src/views/LoginView.vue
+++ game_store_frontend/src/views/LoginView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-const keycloakLoginUrl = 
-    `${import.meta.env.VITE_KEYCLOAK_URL}/realms/game-store/protocol/openid-connect/auth` +
+const appUrl = import.meta.env.VITE_APP_URL || 'http://localhost:5173'
+const keycloakUrl = import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:8080'
+
+const keycloakLoginUrl =
+    `${keycloakUrl}/realms/game-store/protocol/openid-connect/auth` +
     `?client_id=game-store-client` +
-    `&redirect_uri=${import.meta.env.VITE_APP_URL}/login/callback` +
+    `&redirect_uri=${encodeURIComponent(appUrl + '/login/callback')}` +
     `&response_type=code` +
     `&scope=openid`
 
